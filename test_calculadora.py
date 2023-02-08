532619f82b29326902b6fa8ec403047089865820
jorged314159 <jorged314159@gmail.com>
2023-02-08 14:04:30 -0600

    Se añade prueba 10 mas 2

--- test_calculadora.py
+++ test_calculadora.py
@@ -7,6 +7,10 @@ class TestCalculadora(unittest.TestCase):
         calc = Calculadora()
         self.assertEqual(4, calc.suma(2, 2))
 
+    def test_suma_10_mas_2(self):
+        calc = Calculadora()
+        self.assertEqual(11, calc.suma(10, 2))
+
 if __name__ == '__main__':
     unittest.main()
 
